c33da9e4a4f8135c8638583f1f6a9fc65fd661f8
wsadmin <support@sulger.ch>
2024-10-29 20:23:04 +0100

    5-6 OK?

--- src/app/shared/app.routes.ts
+++ src/app/shared/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { categoriesPath } from '../category/category.routes';
 import { expensesPath } from '../expense/expense.routes';
+import { authGuard } from './guard/auth.guard';
 
 export const loginPath = 'login';
 export const defaultPath = categoriesPath; // TODO: switch to ex
@@ -18,6 +19,20 @@ const appRoutes: Routes = [
   {
     path: expensesPath,
     loadChildren: () => import('../expense/expense.routes')
+  },
+  {
+    path: loginPath,
+    loadComponent: () => import('./component/login/login.component')
+  },
+  {
+    path: categoriesPath,
+    loadChildren: () => import('../category/category.routes'),
+    canActivate: [authGuard]
+  },
+  {
+    path: expensesPath,
+    loadChildren: () => import('../expense/expense.routes'),
+    canActivate: [authGuard]
   },
   {
     path: '**',
