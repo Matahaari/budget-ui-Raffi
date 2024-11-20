d6214c79043131360fb81a1137eb9a145de442b5
wsadmin <support@sulger.ch>
2024-11-20 18:18:48 +0100

    finished expense.service.ts - possibly with errors

---
+++ src/app/expense/service/expense.service.ts
@@ -0,0 +1,28 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AllCategoryCriteria, Category, CategoryCriteria, ExpenseUpsertDto, Page } from '../../shared/domain';
+import { environment } from '../../../environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class ExpenseService {
+  private readonly httpClient = inject(HttpClient);
+  private readonly apiUrl = `${environment.backendUrl}/expenses`;
+  private readonly apiV2Url = `${environment.backendUrl}/v2/expenses`;
+
+  // Read
+
+  getExpenses = (pagingCriteria: CategoryCriteria): Observable<Page<Category>> =>
+    this.httpClient.get<Page<Category>>(this.apiUrl, { params: new HttpParams({ fromObject: { ...pagingCriteria } }) });
+
+  /* getAllExpenses = (sortCriteria: AllCategoryCriteria): Observable<Category[]> =>
+    this.httpClient.get<Category[]>(this.apiV2Url, { params: new HttpParams({ fromObject: { ...sortCriteria } }) });*/
+
+  // Create & Update
+
+  upsertExpense = (category: ExpenseUpsertDto): Observable<void> => this.httpClient.put<void>(this.apiUrl, category);
+
+  // Delete
+
+  deleteExpense = (id: string): Observable<void> => this.httpClient.delete<void>(`${this.apiUrl}/${id}`);
+}
